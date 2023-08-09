e9eaab0e44a374e64454584c7991baa4df718993
Nguyen Huy Khai <nguyenhuykhaipch@gmail.com>
2023-08-09 19:00:58 +0700

    First commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+
+// epress app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routers
+app.use("/api/workouts", workoutRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONG_URI)
+  .then(() => {
+    // listten for request
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
